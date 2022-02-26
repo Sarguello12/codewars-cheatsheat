8838002ac029366d61198580479b4061c99df36d
Samuel Arguello <sam.rene12@gmail.com>
2022-02-26 09:35:20 -0600

    completed codewars kata reverse str

--- codewars-cheatsheet.js
+++ codewars-cheatsheet.js
@@ -76,3 +76,15 @@ function printer_error(str){
     return "" + totalOverM + "/" + str.length;
 }
 
+
+// Given a string str, reverse it omitting all non-alphabetic characters.
+//     Example
+//
+// For str = "krishan", the output should be "nahsirk".
+//
+//For str = "ultr53o?n", the output should be "nortlu".
+
+function reverseLetter(str) {
+    let cleanStr = str.replace(/[\W0-9._]+/g, '');
+    return cleanStr.split("").reverse().join("");
+}
